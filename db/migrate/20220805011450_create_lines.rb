22de5953d75565b76785d0cc3ff63e1fac91125e
Gina Mirando <gina.mirando5@gmail.com>
2022-08-26 14:47:43 -0500

    fixed seeds and schema

--- db/migrate/20220805011450_create_lines.rb
+++ db/migrate/20220805011450_create_lines.rb
@@ -1,7 +1,7 @@
 class CreateLines < ActiveRecord::Migration[7.0]
   def change
     create_table :lines do |t|
-      t.string :color
+      t.string :name
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_12_005041) do
   enable_extension "plpgsql"
 
   create_table "lines", force: :cascade do |t|
-    t.string "color"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
