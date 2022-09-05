246269e2d620f9e15ce258965f228c2176bb8da5
Gina Mirando <gina.mirando5@gmail.com>
2022-09-04 22:54:40 -0500

    update seeds, schema, migrate

--- db/migrate/20220812005030_create_stops.rb
+++ db/migrate/20220812005030_create_stops.rb
@@ -2,7 +2,7 @@ class CreateStops < ActiveRecord::Migration[7.0]
   def change
     create_table :stops do |t|
       t.string :name
-      t.integer :id
+      t.integer :parent_id
       t.integer :line_id
       t.integer :stop_id
       t.boolean :is_accessible
